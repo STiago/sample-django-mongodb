94b71d3c8ab5b95d197b9e9376d7633e516c88e8
STiago <victoriasantiago@correo.ugr.es>
2017-04-05 20:03:28 +0200

    Adding project

---
+++ my_website/candidates/urls.py
@@ -0,0 +1,10 @@
+#candidates/urls.py
+
+from django.conf.urls import url
+
+from . import views
+
+urlpatterns = [
+  url(r'^$', views.index, name='index'),
+  url(r'^test/$', views.test, name='test'),
+]
